1eb65070d24fcef9a15b58416668b93bcb2a4c27
Niq11 <juicesky11@gmail.com>
2024-06-03 19:00:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let score = 0;
 
 document.querySelector(".score").textContent = score;
 
-fetch("./data/cards.json")
+fetch("data/cards.json")
   .then((res) => res.json())
   .then((data) => {
     cards = [...data, ...data];

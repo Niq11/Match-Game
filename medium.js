16fc734223d6150a20074ec68355602fc89a3d42
Niq11 <juicesky11@gmail.com>
2024-06-30 23:14:46 +0800

    Update medium.js

--- medium.js
+++ medium.js
@@ -6,7 +6,7 @@ let score = 0;
 
 document.querySelector(".score").textContent = score;
 
-fetch("data/cardmedium.json")
+fetch("data/cards.json")
   .then((res) => res.json())
   .then((data) => {
     cards = [...data, ...data];
